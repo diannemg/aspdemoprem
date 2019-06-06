5115271ab7046724df71b878632c5d3aebe51c8a
Ubuntu <diannemg@me.com>
2019-06-06 15:21:42 +0000

    attach change

--- virtdbnx_db/attach-db.sh
+++ virtdbnx_db/attach-db.sh
@@ -2,7 +2,7 @@
 #wait for the SQL Server to come up
 sleep 10
 #run TSQL attach.sql
-#/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My_P@ssw0rd! -d master -Q "USE [master];CREATE DATABASE [virtdbnxContext] ON (FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext.mdf'),(FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext_log.ldf') for ATTACH;"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My_P@ssw0rd! -d master -Q "USE [master];CREATE DATABASE [virtdbnxContext] CONTAINMENT = NONE ON PRIMARY ( NAME = N'virtdbnxContext', FILENAME = N'/var/opt/mssql/sqldata/udb/virtdbnxContext.mdf', SIZE = 10GB , MAXSIZE = UNLIMITED, FILEGROWTH = 5056KB ) LOG ON ( NAME = N'virtdbnxContext_log', FILENAME = N'/var/opt/mssql/sqldata/udb/virtdbnxContext_log.ldf' , SIZE = 4GB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My_P@ssw0rd! -d master -Q "USE [master];CREATE DATABASE [virtdbnxContext] ON (FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext.mdf'),(FILENAME = '/var/opt/mssql/sqldata/udb/virtdbnxContext_log.ldf') for ATTACH;"
+#/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P My_P@ssw0rd! -d master -Q "USE [master];CREATE DATABASE [virtdbnxContext] CONTAINMENT = NONE ON PRIMARY ( NAME = N'virtdbnxContext', FILENAME = N'/var/opt/mssql/sqldata/udb/virtdbnxContext.mdf', SIZE = 10GB , MAXSIZE = UNLIMITED, FILEGROWTH = 5056KB ) LOG ON ( NAME = N'virtdbnxContext_log', FILENAME = N'/var/opt/mssql/sqldata/udb/virtdbnxContext_log.ldf' , SIZE = 4GB , MAXSIZE = UNLIMITED, FILEGROWTH = 10%)"
 #Keep Alive
 while true; do sleep 1; done
