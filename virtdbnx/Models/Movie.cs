6bfbf31e1e5ee9a538b4b8299c3d140286ccad47
Ubuntu <diannemg@me.com>
2019-06-15 14:50:49 +0000

    error checking

--- virtdbnx/Models/Movie.cs
+++ virtdbnx/Models/Movie.cs
@@ -11,26 +11,26 @@ namespace virtdbnx.Models
         public int ID { get; set; }
 
       //  [StringLength(60, MinimumLength = 2)]
-      //  [Required]
-      //  public string Title { get; set; }
+          [Required]
+          public string Title { get; set; }
 
-      //  [Display(Name = "Release Date")]
-      //  [DataType(DataType.Date)]
-      //  [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-      //  public DateTime ReleaseDate { get; set; }
+          [Display(Name = "Release Date")]
+          [DataType(DataType.Date)]
+          [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+          public DateTime ReleaseDate { get; set; }
 
       //  [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
       //  [Required]
       //  [StringLength(30)]
-      //  public string Genre { get; set; }
+          public string Genre { get; set; }
 
-      //  [Range(1, 100)]
-      //  [Column(TypeName = "decimal(18, 2)")]
-      //  public decimal Price { get; set; }
+          [Range(1, 100)]
+          [Column(TypeName = "decimal(18, 2)")]
+          public decimal Price { get; set; }
 
       //  [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-      //  [StringLength(5)]
+          [StringLength(6)]
       //  [Required]
-      //  public string Rating { get; set; }
+          public string Rating { get; set; }
     }
 }
