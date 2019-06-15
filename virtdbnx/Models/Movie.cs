b2d20a5e0616aac294c759a51ccc4fb350be1ab6
Ubuntu <diannemg@me.com>
2019-06-15 14:26:44 +0000

    remove error

--- virtdbnx/Models/Movie.cs
+++ virtdbnx/Models/Movie.cs
@@ -10,27 +10,27 @@ namespace virtdbnx.Models
     {
         public int ID { get; set; }
 
-        [StringLength(60, MinimumLength = 2)]
-        [Required]
-        public string Title { get; set; }
+      //  [StringLength(60, MinimumLength = 2)]
+      //  [Required]
+      //  public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ReleaseDate { get; set; }
+      //  [Display(Name = "Release Date")]
+      //  [DataType(DataType.Date)]
+      //  [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+      //  public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
-        public string Genre { get; set; }
+      //  [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+      //  [Required]
+      //  [StringLength(30)]
+      //  public string Genre { get; set; }
 
-        [Range(1, 100)]
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
+      //  [Range(1, 100)]
+      //  [Column(TypeName = "decimal(18, 2)")]
+      //  public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
-        public string Rating { get; set; }
+      //  [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+      //  [StringLength(5)]
+      //  [Required]
+      //  public string Rating { get; set; }
     }
 }
